bb4903f4390fd930afc15611c4d00d2a359dc2f8
Zirondev <128257929+ZironDev@users.noreply.github.com>
2024-03-29 20:03:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ prefix = "."
 USER_TOKEN = ""
 client = commands.Bot(command_prefix=prefix, case_insensitive=True, self_bot=True, intents=intents)
 client.remove_command('help')
-client.load_extension('DndCog')
+client.load_extension('AfkCog')
     
 
 @client.event
